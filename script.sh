6e1f9442a0b4626e781b978cf414ed3504b7bc56
chenzhehao <chenzhehao2008666@icloud.com>
2015-12-20 10:29:33 +0800

    test
    test

---
+++ script.sh
@@ -0,0 +1,22 @@
+#zhushi
+
+#path
+patch_begin=/Users/chenzhehao
+
+#display fun
+display_menu()
+{
+	echo "初步验证一下*"
+	echo "显示正确吧*"
+}
+
+while :
+do
+	read vara
+	if [ $# -eq 0 ]
+	then
+	display_menu
+	else
+	echo "不清楚这么执行的"
+	fi
+done
